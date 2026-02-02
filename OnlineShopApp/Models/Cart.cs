3a5a455d2e932d95d97f750b4e248d732ec1044f
PolAleks <aa.polushkin@yandex.ru>
2026-02-02 15:11:22 +0300

    Переименовал Card в Cart

---
+++ OnlineShopApp/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShopApp.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public string? UserId { get; set; }
+        public List<CartItem>? Items { get; set; }
+        public decimal? TotalCost => Items?.Sum(item => item.Cost);
+    }
+}
---
+++ OnlineShopApp/Models/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShopApp.Models
+{
+    public class CartItem
+    {
+        public Guid Id { get; set; }
+        public Product? Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal? Cost => Product?.Cost * Quantity;
+    }
+}
