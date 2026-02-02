d693892bbb941da37be8b51d664a50d1cd3b2643
PolAleks <aa.polushkin@yandex.ru>
2026-02-02 15:13:32 +0300

    Реализовал контроллер Cart и репозиторий CartRepository

---
+++ OnlineShopApp/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopApp.Repositories;
+
+namespace OnlineShopApp.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            var card = CartRepository.TryGetbyUserId(Constans.UserId);
+            return View(card);
+        }
+    }
+}
---
+++ OnlineShopApp/Repositories/CartRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Repositories
+{
+    public static class CartRepository
+    {
+        private static readonly List<Cart> _card = [];
+
+        public static Cart? TryGetbyUserId(string userId)
+        {
+            return _card.FirstOrDefault(c => c.UserId == userId);
+        }
+    }
+}
