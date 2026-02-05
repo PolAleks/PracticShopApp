4ad35089284d263b79ea449f249d71d93a5815a1
PolAleks <aa.polushkin@yandex.ru>
2026-02-05 13:19:35 +0300

    Реализовал отображение представления с отображением заказа

---
+++ OnlineShopApp/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopApp.Interfaces;
+
+namespace OnlineShopApp.Controllers
+{
+    public class OrderController(ICartsRepository cartsRepository) : Controller
+    {
+        private readonly ICartsRepository _cartsRepository = cartsRepository;
+        public IActionResult Index()
+        {
+            var cart = _cartsRepository.TryGetByUserId(Constans.UserId);
+
+            return View(cart);
+        }
+    }
+}
