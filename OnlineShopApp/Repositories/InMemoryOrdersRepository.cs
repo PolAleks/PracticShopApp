40bb7fa89d8b97eb4bf4942702e2ec6fe6792686
PolAleks <aa.polushkin@yandex.ru>
2026-02-05 14:36:25 +0300

    Реализован репозиторий для операций с заказами(Orders)

---
+++ OnlineShopApp/Repositories/InMemoryOrdersRepository.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShopApp.Interfaces;
+using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Repositories
+{
+    public class InMemoryOrdersRepository : IOrdersRepository
+    {
+        private readonly List<Order> _orders = [];
+        public void Add(Order order)
+        {
+            if (order is not null)
+            {
+                order.Id = Guid.NewGuid();
+                _orders.Add(order);
+            }
+        }
+    }
+}
