115f7466d639f7d4eae4cd73d5a21b315e03aef2
PolAleks <aa.polushkin@yandex.ru>
2026-02-06 11:03:51 +0300

    Реализовал интерфейс репозитория IWishlistRepository для работы с избранными товарами

---
+++ OnlineShopApp/Interfaces/IWishlistsRepository.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Interfaces
+{
+    public interface IWishlistsRepository
+    {
+        Wishlist? TryGetWishkistbyUserId(string userId);
+        void Add(int productId);
+        void Remove(int productId);
+        void Clear(string userId);
+    }
+}
