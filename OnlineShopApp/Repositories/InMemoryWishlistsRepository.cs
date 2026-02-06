2fc812fbcf969963b8d4779479df26cb96e84a2c
PolAleks <aa.polushkin@yandex.ru>
2026-02-06 11:34:50 +0300

    Реализован сервис репозиторий работы с товарами в избранном InMemoryWishlistsRepository

---
+++ OnlineShopApp/Repositories/InMemoryWishlistsRepository.cs
@@ -0,0 +1,55 @@
+﻿using OnlineShopApp.Interfaces;
+using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Repositories
+{
+    public class InMemoryWishlistsRepository : IWishlistsRepository
+    {
+        private readonly List<Wishlist> _wishlists = [];
+        public void Add(Product product, string userId)
+        {
+            var existingWishlist = TryGetWishlistByUserId(userId);
+            if (existingWishlist is null)
+            {
+                _wishlists.Add(new Wishlist
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = userId,
+                    Products = [product]
+                });
+            }
+            else
+            {
+                existingWishlist.Products.Add(product);
+            }
+        }
+
+        public void Clear(string userId)
+        {
+            var existingWishlist = _wishlists.FirstOrDefault(w => w.UserId == userId);
+            if (existingWishlist is not null)
+            {
+                _wishlists.Remove(existingWishlist);
+            }
+        }
+
+        public void Remove(Product product, string userId)
+        {
+            var existingWishlist = TryGetWishlistByUserId(userId);
+
+            if (existingWishlist is not null)
+            {
+                var isWishlist = existingWishlist.Products.Contains(product);
+                if (isWishlist)
+                {
+                    existingWishlist.Products.Remove(product);
+                }
+            }
+        }
+
+        public Wishlist? TryGetWishlistByUserId(string userId)
+        {
+            return _wishlists.FirstOrDefault(w => w.UserId == userId);
+        }
+    }
+}
