ab374b19b1f4b95f4f69c7cb0b60a8b9c95b3d22
PolAleks <aa.polushkin@yandex.ru>
2026-02-08 11:38:26 +0300

    Реализовал компонет представления для корзины

---
+++ OnlineShopApp/Views/Shared/Components/Cart/CartViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopApp.Interfaces;
+
+namespace OnlineShopApp.Views.Shared.Components.Cart
+{
+    public class CartViewComponent(ICartsRepository cartsRepository) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var cart = cartsRepository.TryGetByUserId(Constans.UserId);
+            var countProduct = cart?.Quantity ?? 0;
+
+            return View(nameof(Cart), countProduct);
+        }
+        
+    }
+}
