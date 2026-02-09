81c5e11c5ce276e2ece3cddfab219243b37c8069
PolAleks <aa.polushkin@yandex.ru>
2026-02-09 16:42:46 +0300

    Добавлен контролер AccountController

---
+++ OnlineShopApp/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Authorization = OnlineShopApp.Models.Authorization;
+
+namespace OnlineShopApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Authorization()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Authorization(Authorization authorization)
+        {
+            return RedirectToAction(nameof(Index), nameof(HomeController).Replace("Controller", ""));
+        }
+
+        [HttpPost]
+        public IActionResult Registration()
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
