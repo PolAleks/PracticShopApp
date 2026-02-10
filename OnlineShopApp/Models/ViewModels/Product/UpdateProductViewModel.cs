dcbd04f4a556f96c0a8a1debf99494d4a54e716f
PolAleks <aa.polushkin@yandex.ru>
2026-02-10 16:00:44 +0300

    Реализовал модель для представления редактирования товара UpdateProductViewModel

---
+++ OnlineShopApp/Models/ViewModels/Product/UpdateProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShopApp.Models.ViewModels.Product
+{
+    public class UpdateProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost{ get; set; }
+        public string Description { get; set; }
+    }
+}
