9416654b5278d9e8c2a0d0835a4282b3cc89b393
PolAleks <aa.polushkin@yandex.ru>
2026-02-11 18:22:47 +0300

    Отредактировал модель Product

--- OnlineShopApp/Models/Product.cs
+++ OnlineShopApp/Models/Product.cs
@@ -1,18 +1,20 @@
 ﻿namespace OnlineShopApp.Models
 {
-    public class Product(int id, string name, decimal cost, string description)
+    public class Product
     {
-        public int Id { get; set; } = id;
-        public string Name { get; set; } = name;
-        public decimal Cost { get; set; } = cost;
-        public string? Description { get; set; } = description;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; } 
+        public string? Description { get; set; } 
         public string PhotoPath { get; set; } = "/img/product.png";
 
-        public override string ToString()
+        public Product(int id, string name, decimal cost, string? description)
         {
-            return $"Код товара: {Id}{Environment.NewLine}" +
-                $"Наименование: {Name}{Environment.NewLine}" +
-                $"Цена: {Cost:c}{Environment.NewLine}";
+            Id = id;
+            Name = name;
+            Cost = cost;
+            Description = description;
         }
+        public Product() { }
     }
 }
