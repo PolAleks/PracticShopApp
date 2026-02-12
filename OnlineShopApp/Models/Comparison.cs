1a4130d10885166c1d7143cad0b4364cf45f834a
PolAleks <aa.polushkin@yandex.ru>
2026-02-12 15:29:11 +0300

    Реализовал модель Comparison для хранения товаров в списке для сравнения

---
+++ OnlineShopApp/Models/Comparison.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopApp.Models
+{
+    public class Comparison
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public List<Product> Items { get; set; }
+    }
+}
