71504a736c835b46db346d50945340893d1aa684
PolAleks <aa.polushkin@yandex.ru>
2026-02-12 15:32:27 +0300

    Реализовал IComparison и его имплементацию в InMemoryComparisonsRepository

---
+++ OnlineShopApp/Interfaces/IComparisonRepository.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Interfaces
+{
+    public interface IComparisonRepository
+    {
+        Comparison? TryGetByUserId(string userId);
+        void Add(Product product, string userId);
+        void Delete(int productId, string userId);
+        void Clear(string userId);
+    }
+}
---
+++ OnlineShopApp/Repositories/InMemoryComparisonRepository.cs
@@ -0,0 +1,59 @@
+﻿using OnlineShopApp.Interfaces;
+using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Repositories
+{
+    public class InMemoryComparisonRepository : IComparisonRepository
+    {
+        private readonly List<Comparison> _comparisons = [];
+
+        public void Add(Product product, string userId)
+        {
+            var comparison = TryGetByUserId(userId);
+
+            if (comparison is null)
+            {
+                comparison = new Comparison
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = userId,
+                    Items = new List<Product> { product }
+                };
+                _comparisons.Add(comparison);
+            }
+            else
+            {
+                if (!comparison.Items.Any(p => p.Id == product.Id))
+                {
+                    comparison.Items.Add(product);
+                }
+            }
+        }
+
+        public void Clear(string userId)
+        {
+            var comparison = TryGetByUserId(userId);
+            if (comparison is not null)
+            {
+                _comparisons.Remove(comparison);
+            }
+        }
+
+        public void Delete(int productId, string userId)
+        {
+            var comparison = TryGetByUserId(userId);
+
+            var product = comparison?.Items.FirstOrDefault(p => p.Id == productId);
+
+            if (product is not null)
+            {
+                comparison!.Items.Remove(product);
+            }
+        }
+
+        public Comparison? TryGetByUserId(string userId)
+        {
+            return _comparisons.FirstOrDefault(c => c.UserId == userId);
+        }
+    }
+}
