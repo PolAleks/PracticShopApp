ba6ed77c47b781b7c06de5ce4dbfc5cc1ab364ee
PolAleks <aa.polushkin@yandex.ru>
2026-02-16 15:58:22 +0300

    Добавил атрибуты валидации в модель Product

--- OnlineShopApp/Models/Product.cs
+++ OnlineShopApp/Models/Product.cs
@@ -1,10 +1,25 @@
-﻿namespace OnlineShopApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopApp.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Display(Name = "Наименование товара", Prompt = "Наименование товара")]
+        [Required(ErrorMessage = "Не указано наименование товара")]
+        [DataType(DataType.Text)]
+        [StringLength(200, ErrorMessage = "Наименование товара должно содержать от {2} до {1} символов", MinimumLength = 2)]
         public string Name { get; set; }
-        public decimal Cost { get; set; } 
+
+        [Display(Name = "Цена, руб.", Prompt = "Цена, руб.")]
+        [Required(ErrorMessage = "Не указана цена товара")]
+        [Range(0, 1000000, ErrorMessage = "Стоимость должна быть в диапазоне от {1} до {2:c}")]
+        public decimal Cost { get; set; }
+
+        [Display(Name = "Описание товара", Prompt = "Описание товара")]
+        [MaxLength(4069, ErrorMessage = "Описание товара не должно превышать {1} символов")]
+        [DataType(DataType.MultilineText)]
         public string? Description { get; set; } 
         public string PhotoPath { get; set; } = "/img/product.png";
 
