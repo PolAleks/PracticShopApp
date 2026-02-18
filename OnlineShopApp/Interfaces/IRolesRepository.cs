31d1cf9907d552bf7d70fa3343ed7587920a807b
PolAleks <aa.polushkin@yandex.ru>
2026-02-18 10:36:49 +0300

    Реализовал репозиторий ролей

---
+++ OnlineShopApp/Interfaces/IRolesRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Interfaces
+{
+    public interface IRolesRepository
+    {
+        List<Role> GetAll();
+        Role? TryGetById(Guid id);
+        Role? TryGetByName(string name);
+        void Add(Role role);
+        void Delete(Guid id);
+    }
+}
