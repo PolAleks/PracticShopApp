f260b80ba6401a2e68a4f2d3914411058af2bbda
PolAleks <aa.polushkin@yandex.ru>
2026-02-18 10:36:21 +0300

    Добавил модель Role

---
+++ OnlineShopApp/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopApp.Models
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+
+        [Display(Name = "Роль", Prompt = "Введите новую роль")]
+        [Required(ErrorMessage = "Не указано название роли")]
+        [StringLength(50, ErrorMessage = "Роль должна содержать от {2} до {1} символов", MinimumLength = 2)]
+        [DataType(DataType.Text)]
+        public string Name { get; set; } = null!;
+    }
+}
