c650788f990021cc08ea4960602f40ef3fbf50af
PolAleks <aa.polushkin@yandex.ru>
2026-02-21 10:09:50 +0300

    Реализован репозиторий для работы с Users

---
+++ OnlineShopApp/Repositories/InMemoryUsersRepository.cs
@@ -0,0 +1,37 @@
+﻿using OnlineShopApp.Interfaces;
+using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Repositories
+{
+    public class InMemoryUsersRepository : IUsersRepository
+    {
+        private readonly List<User> _users = [];
+        public void Add(User newUser)
+        {
+            newUser.Id = Guid.NewGuid();
+            newUser.CreationDateTime = DateTime.Now;
+
+            _users.Add(newUser);
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User? TryGetByLogin(string login)
+        {
+            return _users.FirstOrDefault(u => u.Login == login);
+        }
+
+        public void Update(Guid id, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRole(Guid id, Role newRole)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
