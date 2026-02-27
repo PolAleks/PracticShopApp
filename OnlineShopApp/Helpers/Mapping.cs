9a9a8aca199e0ed9d3fb0b2fe8ec7671054cb018
PolAleks <aa.polushkin@yandex.ru>
2026-02-27 12:37:46 +0300

    Реализовал маппинг для преобразования Product=>ProductViewModel и наоборот

---
+++ OnlineShopApp/Helpers/Mapping.cs
@@ -0,0 +1,44 @@
+﻿using OnlineShop.Db.Models;
+using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Helpers
+{
+    public static class Mapping
+    {
+        public static IEnumerable<ProductViewModel> ToViewModels(this IEnumerable<Product> products)
+        {
+            var productsViewModel = new List<ProductViewModel>();
+
+            foreach (var product in products)
+            {
+                productsViewModel.Add(product.ToViewModel());
+            }
+
+            return productsViewModel;
+        }
+
+        public static ProductViewModel ToViewModel(this Product product)
+        {
+            return new ProductViewModel()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Cost = product.Cost,
+                Description = product.Description,
+                PhotoPath = product.PhotoPath
+            };
+        }
+
+        public static Product ToProductDb(this ProductViewModel productViewModel)
+        {
+            return new Product()
+            {
+                Id = productViewModel.Id,
+                Name = productViewModel.Name,
+                Cost = productViewModel.Cost,
+                Description = productViewModel.Description,
+                PhotoPath = productViewModel.PhotoPath
+            };
+        }
+    }
+}
