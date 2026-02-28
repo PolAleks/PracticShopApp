af74e2294ac8a0d53cb04c3cd863f0d002422229
PolAleks <aa.polushkin@yandex.ru>
2026-02-28 12:29:54 +0300

    Создал конфигурацию моделей Product, Cart и CartItem с помощью Fluent Api в отдельных классах

---
+++ OnlineShop.Db/Configurations/CartConfigurarion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db.Configurations
+{
+    public class CartConfigurarion : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.ToTable("carts");
+
+            builder.HasKey(cart => cart.Id);
+
+            builder.Property(cart => cart.Id)
+                .HasColumnName("cart_id")
+                .ValueGeneratedNever(); // Запрет генерации GUID в БД
+
+            builder.Property(cart => cart.UserId)
+                .IsRequired()
+                .HasColumnName("user_id");
+
+            // Настройка связи с CartItem (один-ко-многим)
+            builder.HasMany(c => c.Items)
+                .WithOne(ci => ci.Cart)
+                .HasForeignKey(ci => ci.CartId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ OnlineShop.Db/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db.Configurations
+{
+    public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.ToTable("cart_items");
+
+            builder.HasKey(ci => ci.Id);
+
+            builder.Property(ci => ci.Id)
+                .HasColumnName("cart_item_id")
+                .ValueGeneratedNever(); // Запрет генерации GUID в БД
+
+            builder.Property(ci => ci.Quantity)
+                .HasColumnName("quantity")
+                .IsRequired()
+                .HasDefaultValue(1);
+
+            builder.Property(ci => ci.CartId)
+                .HasColumnName("cart_id")
+                .IsRequired();
+
+            builder.Property(ci => ci.ProductId)
+                .HasColumnName("product_id");
+
+            // Настройка связи с Product (один-ко-многим)
+            builder.HasOne(ci => ci.Product)
+                .WithMany(p => p.CartItems)
+                .HasForeignKey(ci => ci.ProductId)
+                .OnDelete(DeleteBehavior.Restrict); // Не удалять Product при удалении CartItem
+
+            // Настройка связи с Cart (один комногим)
+            builder.HasOne(ci => ci.Cart)
+                .WithMany(cart => cart.Items)
+                .HasForeignKey(ci => ci.CartId)
+                .OnDelete(DeleteBehavior.Cascade); // Удалять CartItem при удалении Cart
+
+        }
+    }
+}
