5fc5ca21fe044102c77f40dcb9aa90037b6bf068
PolAleks <aa.polushkin@yandex.ru>
2026-02-28 15:34:27 +0300

    Реализовал репозитория CartsDbRepository

---
+++ OnlineShop.Db/Interfaces/ICartsRepository.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db.Interfaces
+{
+    public interface ICartsRepository
+    {
+        Cart? TryGetByUserId(string userId);
+        void Add(Product product, string userId);
+        void Subtract(int productId, string userId);
+        void Clear(string userId);
+    }
+}
---
+++ OnlineShop.Db/Repositories/CartsDbRepository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Db.Interfaces;
+using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db.Repositories
+{
+    public class CartsDbRepository(DatabaseContext databaseContext) : ICartsRepository
+    {
+
+        public Cart? TryGetByUserId(string userId)
+        {
+            return databaseContext.Carts
+                .Include(cart => cart.Items)
+                .ThenInclude(cartItem => cartItem.Product)
+                .FirstOrDefault(cart => cart.UserId == userId);
+        }
+
+        public void Add(Product product, string userId)
+        {
+            var existingCart = TryGetByUserId(userId);
+
+            if (existingCart is null)
+            {
+                Cart newCart = new()
+                {
+                    UserId = userId,
+                    Items = []
+                };
+
+                newCart.Items = [ new CartItem()
+                {
+                    Product = product,
+                    Quantity = 1
+                } ];
+
+                databaseContext.Carts.Add(newCart);
+            }
+            else
+            {
+                var existingItem = existingCart.Items?.FirstOrDefault(item => item.Product.Id == product.Id);
+
+                if (existingItem is null)
+                {
+                    CartItem item = new()
+                    {
+                        Cart = existingCart,
+                        Product = product,
+                        Quantity = 1
+                    };
+
+                    databaseContext.CartItems.Add(item);
+                }
+                else
+                {
+                    existingItem.Quantity++;
+                }
+            }
+
+            databaseContext.SaveChanges();
+        }
+
+        public void Subtract(int productId, string userId)
+        {
+            var existingCart = TryGetByUserId(userId);
+
+            var existingCartItem = existingCart?.Items?.FirstOrDefault(item => item.Product?.Id == productId);
+
+            if (existingCartItem is null)
+                return;
+
+            existingCartItem.Quantity--;
+
+            if (existingCartItem.Quantity == 0)
+                existingCart?.Items?.Remove(existingCartItem);
+
+            databaseContext.SaveChanges();
+        }
+
+        public void Clear(string userId)
+        {
+            var existingCart = TryGetByUserId(userId);
+            if (existingCart is not null)
+            {
+                databaseContext.Carts.Remove(existingCart);
+                databaseContext.SaveChanges();
+            }
+        }
+    }
+}
