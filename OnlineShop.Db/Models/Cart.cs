5b280cc23ba7670badb04c42f7a71e9205a9f147
PolAleks <aa.polushkin@yandex.ru>
2026-02-28 12:26:37 +0300

    Реализовал модель Cart и Cartitem для БД

---
+++ OnlineShop.Db/Models/Cart.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShop.Db.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public required string UserId { get; set; }
+
+        // Навигационные свойства
+        public ICollection<CartItem>? Items { get; set; }
+    }
+}
---
+++ OnlineShop.Db/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿namespace OnlineShop.Db.Models
+{
+    public class CartItem
+    {
+        public Guid Id { get; set; }
+        public int Quantity { get; set; }
+
+        // Внешние ключи
+        public Guid CartId { get; set; }
+        public int ProductId { get; set; }
+
+        // Навигационные свойства
+        public required Product Product { get; set; }
+        public Cart? Cart { get; set; }
+    }
+}
