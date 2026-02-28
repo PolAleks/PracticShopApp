24d8b0dd7b44a752289b68820a2c3d35d2bb28fe
PolAleks <aa.polushkin@yandex.ru>
2026-02-28 15:38:15 +0300

    Переключил CartController на работу с БД

--- OnlineShopApp/Controllers/CartController.cs
+++ OnlineShopApp/Controllers/CartController.cs
@@ -1,42 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Db.Interfaces;
 using OnlineShopApp.Helpers;
-using OnlineShopApp.Interfaces;
 
 namespace OnlineShopApp.Controllers
 {
     public class CartController(ICartsRepository cartsRepository, IProductsRepository productsRepository) : Controller
     {
-        private readonly ICartsRepository _cartsRepository = cartsRepository;
-        private readonly IProductsRepository _productsRepository = productsRepository;
 
         public IActionResult Index()
         {
-            var cart = _cartsRepository.TryGetByUserId(Constans.UserId);
+            var cart = cartsRepository.TryGetByUserId(Constans.UserId);
 
-            return View(cart);
+            return View(cart?.ToViewModel());
         }
 
         public IActionResult Add(int productId)
         {
-            var product = _productsRepository.TryGetById(productId);
+            var product = productsRepository.TryGetById(productId);
             if (product is not null)
             {
-                _cartsRepository.Add(product.ToViewModel(), Constans.UserId);
+                cartsRepository.Add(product, Constans.UserId);
             }
             return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Subtract(int productId)
         {
-            _cartsRepository.Subtract(productId, Constans.UserId);
+            cartsRepository.Subtract(productId, Constans.UserId);
 
             return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Clear()
         {
-            _cartsRepository.Clear(Constans.UserId);
+            cartsRepository.Clear(Constans.UserId);
             return RedirectToAction(nameof(Index));
         }
     }
