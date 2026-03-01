1e9f2d2d50453f95afca14448e12cf01fc36fbd8
PolAleks <aa.polushkin@yandex.ru>
2026-03-01 15:27:10 +0300

    Удалил модели Comparison и Favorit с репозитариями для работы в памяти

--- OnlineShopApp/Models/Comparison.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineShopApp.Models
-{
-    public class Comparison
-    {
-        public Guid Id { get; set; }
-        public string UserId { get; set; }
-        public List<ProductViewModel> Items { get; set; }
-    }
-}
