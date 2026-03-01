4fcbe3dec21cddbdfcf183ad1b984b49c405e9dd
PolAleks <aa.polushkin@yandex.ru>
2026-03-01 15:34:49 +0300

    Реализовал ViewModel для Comparison и Favorite

---
+++ OnlineShopApp/Models/ComparisonViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopApp.Models
+{
+    public class ComparisonViewModel
+    {
+        public Guid Id { get; set; }
+        public required string UserId { get; set; }
+        public List<ProductViewModel>? Items { get; set; }
+    }
+}
