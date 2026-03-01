e280afe46faec9ce2ba0becdc2a6dc289fa92d63
PolAleks <aa.polushkin@yandex.ru>
2026-03-01 15:35:49 +0300

    Реализовал мапинг Favorite и Comparison во ViewModel

---
+++ OnlineShopApp/Helpers/ComparisonMapping.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShop.Db.Models;
+using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Helpers
+{
+    public static class ComparisonMapping
+    {
+        public static ComparisonViewModel ToViewModel(this Comparison comparison)
+        {
+            return new ComparisonViewModel()
+            {
+                Id = comparison.Id,
+                UserId = comparison.UserId,
+                Items = comparison.Products?.ToViewModels().ToList()
+            };
+        }
+    }
+}
---
+++ OnlineShopApp/Helpers/FavoriteMapping.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShop.Db.Models;
+using OnlineShopApp.Models;
+
+namespace OnlineShopApp.Helpers
+{
+    public static class FavoriteMapping
+    {
+        public static FavoriteViewModel ToViewModel(this Favorite favorite)
+        {
+            return new FavoriteViewModel()
+            {
+                Id = favorite.Id,
+                UserId = favorite.UserId,
+                Items = favorite.Products?.ToViewModels().ToList()
+            };
+        }
+    }
+}
