a3d46d82ee68da0075d937cc7fd72c63bb46a86f
PolAleks <aa.polushkin@yandex.ru>
2026-03-01 17:38:33 +0300

    Изменил инициализацию свойства FavoriteProducts на коллекцию HashSet

--- OnlineShop.Db/Models/Favorite.cs
+++ OnlineShop.Db/Models/Favorite.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public required string UserId { get; set; }
-        public ICollection<Product>? Products { get; set; }
+        public ICollection<Product> Products { get; set; } = [];
+        public ICollection<FavoriteProduct> FavoriteProducts { get; set; } = new HashSet<FavoriteProduct>();
     }
 }
