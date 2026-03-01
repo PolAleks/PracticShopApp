5a15ce74067a5216fcf44ea7c4b0bed2d38262d2
PolAleks <aa.polushkin@yandex.ru>
2026-03-01 18:37:40 +0300

    Удалил комментарии

--- OnlineShop.Db/Models/Comparison.cs
+++ OnlineShop.Db/Models/Comparison.cs
@@ -5,7 +5,6 @@
         public Guid Id { get; set; }
         public required string UserId { get; set; }
 
-        // Navigation property
         public ICollection<Product> Products { get; set; } = [];
         public ICollection<ComparisonProduct> ComparisonProducts { get; set; } = new HashSet<ComparisonProduct>();
     }
