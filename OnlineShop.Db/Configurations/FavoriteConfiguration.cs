479e10708b56f311e6c7e26565faa271551bcc07
PolAleks <aa.polushkin@yandex.ru>
2026-03-01 15:30:58 +0300

    Реализовал конфигурацию Favorite для БД

---
+++ OnlineShop.Db/Configurations/FavoriteConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db.Configurations
+{
+    public class FavoriteConfiguration : IEntityTypeConfiguration<Favorite>
+    {
+        public void Configure(EntityTypeBuilder<Favorite> builder)
+        {
+            builder.ToTable("favorite");
+
+            builder.HasKey(f => f.Id);
+
+            builder.Property(f => f.Id)
+                .HasColumnName("favorite_id");
+
+            builder.Property(f => f.UserId)
+                .HasColumnName("user_id");
+
+            builder.HasMany(f => f.Products)
+                .WithMany(p => p.Favorites);                
+        }
+    }
+}
