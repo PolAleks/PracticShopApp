3a88bb8300f50fadfd24d8a3899c37862f3c838c
PolAleks <aa.polushkin@yandex.ru>
2026-03-01 15:30:38 +0300

    Реализовал конфигурацию Comparison для БД

---
+++ OnlineShop.Db/Configurations/ComparsionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db.Configurations
+{
+    public class ComparsionConfiguration : IEntityTypeConfiguration<Comparison>
+    {
+        public void Configure(EntityTypeBuilder<Comparison> builder)
+        {
+            builder.ToTable("comparison");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .HasColumnName("comparison_id");
+
+            builder.Property(c => c.UserId)
+                .HasColumnName("user_id");
+
+            builder.HasMany(c => c.Products)
+                .WithMany(p => p.Comparisons);
+        }
+    }
+}
