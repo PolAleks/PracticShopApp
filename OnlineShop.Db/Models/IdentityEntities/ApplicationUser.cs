03bad6373f5f7a23f896aca112ded0f314bc3592
PolAleks <aa.polushkin@yandex.ru>
2026-03-09 19:57:09 +0300

    Добавил CreationDateTime в модель ApplicationUser

--- OnlineShop.Db/Models/IdentityEntities/ApplicationUser.cs
+++ OnlineShop.Db/Models/IdentityEntities/ApplicationUser.cs
@@ -6,5 +6,6 @@ namespace OnlineShop.Db.Models.IdentityEntities
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public DateTime CreationDateTime { get; set; } = DateTime.UtcNow;
     }
 }
