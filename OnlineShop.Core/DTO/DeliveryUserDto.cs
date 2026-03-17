49f1156ce4ad48e925827fc44fb56eb53da5ee5c
PolAleks <aa.polushkin@yandex.ru>
2026-03-17 22:02:07 +0300

    Поправил модель DeliveryUserDto

--- OnlineShop.Core/DTO/DeliveryUserDto.cs
+++ OnlineShop.Core/DTO/DeliveryUserDto.cs
@@ -3,11 +3,11 @@
     public class DeliveryUserDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
         public DateOnly Date { get; set; }
-        public string Comment { get; set; }
-        public OrderDto Order { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public OrderDto Order { get; set; } = new();
     }
 }
