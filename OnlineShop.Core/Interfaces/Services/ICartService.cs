c63579778c82980f17d3a45f2d70ea39029a39e2
PolAleks <aa.polushkin@yandex.ru>
2026-03-19 17:43:23 +0300

    Реализовал метод MergeCartAsync для перемещения содержимого корзины неавторизированного пользователя в корзину авторизированного

--- OnlineShop.Core/Interfaces/Services/ICartService.cs
+++ OnlineShop.Core/Interfaces/Services/ICartService.cs
@@ -4,10 +4,11 @@ namespace OnlineShop.Core.Interfaces.Services
 {
     public interface ICartService
     {
-        Task<CartDto> GetCartAsync(string userId);
-        Task AddToCartAsync(string userId, int productId);
-        Task IncreaseQuantityAsync(string userId, int productId);
-        Task DecreaseQuantityAsync(string userId, int productId);
-        Task ClearCartAsync(string userId);
+        Task<CartDto> GetCartAsync(string userName);
+        Task AddToCartAsync(string userName, int productId);
+        Task IncreaseQuantityAsync(string userName, int productId);
+        Task DecreaseQuantityAsync(string userName, int productId);
+        Task ClearCartAsync(string userName);
+        Task MergeCartAsync(string sourcUserName, string destinationUserName);
     }
 }
