1aaf535515d7798ffb7a355c04a03d12e933a418
PolAleks <aa.polushkin@yandex.ru>
2026-03-25 17:05:39 +0300

    Добавил SaveProductImagesAsync в IImageService

--- OnlineShop.Core/Interfaces/Services/IImageService.cs
+++ OnlineShop.Core/Interfaces/Services/IImageService.cs
@@ -4,7 +4,8 @@ namespace OnlineShop.Core.Interfaces.Services
 {
     public interface IImageService
     {
-        Task<string> SaveImageAsync(IFormFile image, string folder, string fileName);
+        Task<string> SaveImageAsync(IFormFile file, string fileName, string folder);
+        Task<IEnumerable<string>> SaveProductImagesAsync(IEnumerable<IFormFile> files, int productId);
         void DeleteImage(string imagePath);
     }
 }
